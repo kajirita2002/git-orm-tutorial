0be3ce24a5d6346ad6b36a4428def0dc3f3d7b67
rita <kajirita2002@gmail.com>
2021-02-10 03:05:30 +0900

    first commit

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"go-orm-tutorial/entity"
+
+	"github.com/gorilla/mux"
+)
+
+func helloWorld(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "Hello World")
+}
+
+func handleRequest() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", helloWorld).Methods("GET")
+	router.HandleFunc("/users", entity.AllUsers)
+	router.HandleFunc("/user/{name}{email}", entity.NewUser).Methods("POST")
+	router.HandleFunc("/users", entity.DeleteUser).Methods("DELETE")
+	router.HandleFunc("/users", entity.UpdateUser).Methods("PUT")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func main() {
+	fmt.Println("start")
+
+	entity.InitialMigration()
+	handleRequest()
+
+}
